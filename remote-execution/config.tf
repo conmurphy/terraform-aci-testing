a2ad3d98ec325954270d1f69b091a0a284d5e590
Conor Murphy <conormurphy33@hotmail.com>
2020-07-08 22:38:50 +0200

    adding remote config

---
+++ remote-execution/config.tf
@@ -0,0 +1,31 @@
+#configure provider with your cisco aci credentials.
+provider "aci" {
+  # cisco-aci user name
+  username = "${var.username}"
+  # cisco-aci password
+  password = "${var.password}"
+  # cisco-aci url
+  url      =  "${var.url}"
+  insecure = true
+}
+
+
+resource "aci_tenant" "terraform_tenant" {
+  name        = "tenant_for_terraform"
+  description = "This tenant is created by the Terraform ACI provider"
+}
+
+resource "aci_bridge_domain" "bd_for_subnet" {
+  tenant_dn   = "${aci_tenant.terraform_tenant.id}"
+  name        = "bd_for_subnet"
+  description = "This bridge domain is created by the Terraform ACI provider"
+}
+
+resource "aci_subnet" "demosubnet" {
+  bridge_domain_dn                    = "${aci_bridge_domain.bd_for_subnet.id}"
+  ip                                  = "10.1.1.2/24"
+  scope                               = "private"
+  description                         = "This subject is created by Terraform"
+}
+
+variable "apic_url" {}
