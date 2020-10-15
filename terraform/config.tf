c7470e3caa0d7b5149aa40b54610aabd32e79b87
Conor Murphy <conormurphy33@hotmail.com>
2020-10-15 16:09:59 +0200

    Making a change to the subnet description

--- terraform/config.tf
+++ terraform/config.tf
@@ -47,7 +47,7 @@ resource "aci_subnet" "demosubnet" {
   parent_dn                    = "${aci_bridge_domain.bd_for_subnet.id}"
   ip                                  = "172.16.1.1/24"
   scope                               = "private"
-  description                         = "This subject is created by Terraform"
+  description                         = "This subject is created by Terraform v2"
 }
 
 
